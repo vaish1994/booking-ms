9220428bafd8eaffe13a9dffb3660d2db6932a7d
Pallavi Gaonkar <pallavigaonkar4@gmail.com>
2025-02-16 22:54:53 +0530

    coderefactor

---
+++ src/test/java/com/example/booking_ms/BookingMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.booking_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
