9ad6fdacc046641ab3d2812612beb1356e86811a
Pallavi Gaonkar <pallavigaonkar4@gmail.com>
2025-02-27 21:56:07 +0530

    added holiday class

---
+++ src/main/java/com/example/booking_ms/holiday.java
@@ -0,0 +1,9 @@
+package com.example.booking_ms;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class holiday {
+        @GetMapping("/holiday")
+        public String getData() {return "Please book your room for 20% discount" ; }}
