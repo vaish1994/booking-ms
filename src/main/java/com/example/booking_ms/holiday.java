01ce5274521a1b448b79bde028767e808fe6d3a5
vaish1994 <vaishnavigaonkar31@gmail.com>
2025-02-27 22:01:13 +0530

    Update holiday.java

--- src/main/java/com/example/booking_ms/holiday.java
+++ src/main/java/com/example/booking_ms/holiday.java
@@ -6,4 +6,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class holiday {
         @GetMapping("/holiday")
-        public String getData() {return "Please book your room for 20% discount" ; }}
+        public String getData() {return "Please book your room for 20% discount. Discount end at 20 March" ; }}
