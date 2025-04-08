bbd2afad5d09803dfd74e3c2e3bc32f3faaabedb
Pallavi Gaonkar <pallavigaonkar4@gmail.com>
2025-04-08 14:57:34 +0530

    add new file returnback

---
+++ src/main/java/com/example/booking_ms/returnback.java
@@ -0,0 +1,14 @@
+package com.example.booking_ms;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class returnback {
+    @GetMapping("/returnback")
+    public String getData() {return "vacation over!!!" ; }
+}
+
+
+
+
